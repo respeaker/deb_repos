66a41f6d8077cf764ec51922072114f254f1bd77
Baozhu Zuo <zuobaozhu@gmail.com>
2017-08-28 09:36:27 +0800

    respeaker-osconfig: some file setup debian system

---
+++ debian-9-respeaker-osconfig/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="respeaker-osconfig"
+debian_pkg_name="${package_name}"
+package_version="2017.08.28"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="u/${package_name}"
+dl_path=""
+
+debian_version="${package_version}"
+debian_untar=""
+debian_patch=""
+
+stretch_version="~re+20170828+1"
