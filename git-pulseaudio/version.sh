b930942f43d436c886f393bf480cdc4a89f1b5f6
Baozhu Zuo <zuobaozhu@gmail.com>
2017-09-12 15:34:37 +0800

    update pulseaudio version to 11.0.1

--- git-pulseaudio/version.sh
+++ git-pulseaudio/version.sh
@@ -4,12 +4,12 @@
 
 package_name="pulseaudio"
 debian_pkg_name="${package_name}"
-package_version="10.99.1-7-g6921e"
+package_version="11.0.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://anongit.freedesktop.org/pulseaudio/pulseaudio"
-git_sha="6921e08f605bbe7c1e2c2fc1afe35c1202239627"
+git_sha="24928d6b6fa1cde3312bcf63e4e14d7003aadc69"
 reprepro_dir="p/${package_name}"
 dl_path=""
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-0respeaker"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~re+20170825+1"
+stretch_version="~re+20170912+1"
