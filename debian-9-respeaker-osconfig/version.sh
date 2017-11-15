efc76ea9765e535a40fbe80ab56d3094813663cf
Baozhu Zuo <zuobaozhu@gmail.com>
2017-11-15 14:30:53 +0800

    respeaker-osconfig: update version

--- debian-9-respeaker-osconfig/version.sh
+++ debian-9-respeaker-osconfig/version.sh
@@ -2,17 +2,17 @@
 
 package_name="respeaker-osconfig"
 debian_pkg_name="${package_name}"
-package_version="2017.10.26"
+package_version="2017.11.15"
 package_source=""
 src_dir=""
 
 git_repo=""
 git_sha=""
-reprepro_dir="u/${package_name}"
+reprepro_dir="re/${package_name}"
 dl_path=""
 
 debian_version="${package_version}"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~re+20171026+1"
+stretch_version="~re+20171115+1"
