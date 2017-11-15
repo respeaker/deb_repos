9277c6a500eed60f47a07583de385c47fc046417
Baozhu Zuo <zuobaozhu@gmail.com>
2017-11-15 19:15:05 +0800

    debian-9-re-bluetooth: create package

---
+++ debian-9-re-bluetooth/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="re-bluetooth"
+debian_pkg_name="${package_name}"
+package_version="2017.11.15"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="re/${package_name}"
+dl_path=""
+
+debian_version="${package_version}"
+debian_untar=""
+debian_patch=""
+
+stretch_version="~re+20171115+1"
