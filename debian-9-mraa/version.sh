4bd2ad85f7893624495d9b39e8ebb39bbda18e7c
Baozhu Zuo <zuobaozhu@gmail.com>
2017-11-16 18:59:16 +0800

    update mraa version

--- debian-9-mraa/version.sh
+++ debian-9-mraa/version.sh
@@ -4,12 +4,12 @@
 
 package_name="mraa"
 debian_pkg_name="${package_name}"
-package_version="1.8.0-git20170927"
+package_version="1.8.0-git20171030"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/respeaker/mraa"
-git_sha="da7311862ebdb847481bf42361d27252a086017d"
+git_sha="b0fba93dd690eb5d8448affcd281bfe987465463"
 reprepro_dir="libm/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_patch=""
 debian_diff=""
 local_patch="0re2"
 
-stretch_version="~re2+20170923+1"
+stretch_version="~re2+20171030+1"
