942b8735b493151b5cb15080fdcaa747195467bc
turmary <turmary@126.com>
2018-08-27 10:15:19 +0800

    Add: debian upm v1.6.0

--- debian-9-upm/version.sh
+++ debian-9-upm/version.sh
@@ -4,18 +4,18 @@
 
 package_name="upm"
 debian_pkg_name="${package_name}"
-package_version="1.5.0-git20171116"
+package_version="1.6.0-git20180813"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/intel-iot-devkit/upm"
-git_sha="18b8ca26335186c6ea0ae2abf06476938917ecab"
+git_sha="9a959b578cb69a5f0766f415da640bcbd3ebabb1"
 reprepro_dir="u/${package_name}"
 dl_path=""
 
 debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
-local_patch="re2"
+local_patch="0pi"
 
-stretch_version="~re2+20171116+1"
+stretch_version="20180813"
