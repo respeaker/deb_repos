e69df9e1a69a3fd49f1233fdffe655fb9e35ff65
turmary <turmary@126.com>
2019-04-08 11:38:15 +0800

    Fix: BCM 2835 (armv6) need a gcc/libc-dev from Raspbian deb packages source

--- debian-9-mraa/version.sh
+++ debian-9-mraa/version.sh
@@ -4,12 +4,12 @@
 
 package_name="mraa"
 debian_pkg_name="${package_name}"
-package_version="1.9.0-git20181030"
+package_version="1.9.0-git20190404"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://github.com/respeaker/mraa"
-git_sha="816e8e321a6e18885d52bef079ffbae49802ecf4"
+git_repo="-b v1.9.0-grove.py https://github.com/respeaker/mraa"
+git_sha="v1.9.0-grove.py"
 reprepro_dir="libm/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_patch=""
 debian_diff=""
 local_patch="0pi"
 
-stretch_version="20181030"
+stretch_version="20190404"
